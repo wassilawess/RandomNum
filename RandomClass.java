09f54025a5109751a36cc25843394200f8c51f49
wassilawess <benhenia.wassila@gmail.com>
2020-08-25 11:19:08 -0400

    second commit

--- RandomClass.java
+++ RandomClass.java
@@ -2,41 +2,77 @@ package random;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class RandomClass {
 
 	public static void main(String[] args) {
-		
+
 		int start = 0;
-        int end = 500;
-        int counter = 500;
+		int end = 500;
+		int counter = 500;
 
-        int smallest;
-        int randomInt ;
+		int smallest;
+		int randomInt;
 
-        int numbers [] = new int[counter];
-        Random random = new Random(); 
+		int numbers[] = new int[counter];
+		Random random = new Random();
 
-        randomInt = random.nextInt(end-start) + start;
-        numbers [0] = randomInt;
+		randomInt = random.nextInt(end - start) + start;
+		numbers[0] = randomInt;
 
-        smallest = randomInt;
+		smallest = randomInt;
 
 
-        System.out.println("0th Smallest : " + smallest + "\n");
+		for (int i = 1; i < counter; i++) {
 
-        for(int i=1;i<counter;i++) {
+			numbers[i] = random.nextInt(end - start) + start;
 
-            numbers[i] = random.nextInt(end-start) + start;
+			smallest = Math.min(smallest, numbers[i]);
+		}
 
-            smallest = Math.min(smallest,numbers[i]);
-              System.out.println("Random numbers: " + Arrays.toString(numbers) + "\n\n");
+		System.out.println("Random numbers: " + Arrays.toString(numbers) + "\n\n");
 
-            System.out.println(i + "th Smallest : " + smallest + "\n");
+		System.out.println("enter number between 0 and 499 :");
 
-        }
-	
-        
+		Scanner numUser = new Scanner(System.in);
+		int num = numUser.nextInt();
 
+		while (num > (counter - 1)) {
+			System.out.println("enter number between 0 and 499 :");
+		}
+
+		System.out.println(num + "th Smallest : " + smallest + "\n");
+
+		System.out.println("Do you want to choose an other number ?(1/0)");
+
+		int val = numUser.nextInt();
+
+		while (val == 1) {
+			System.out.println("enter number between 0 and 499 :");
+
+			int num1 = numUser.nextInt();
+
+			while (num > (counter - 1)) {
+				System.out.println("enter number between 0 and 499 :");
+			}
+
+			while (num1 > counter) {
+				System.out.println("enter number between 0 and 499!");
+			}
+			
+			System.out.println(num1 + "th Smallest : " + smallest + "\n");
+
+			System.out.println("Do you want to choose an other number ?(1/0)");
+
+			val = numUser.nextInt();
+
+		}
+
+		if (val == 0) {
+			System.out.println("have a geat day!");
+
+		}
+		
 	}
 }
